2b643cc636673f370f1990fd24f4adba66f06141
JSwhiz <dmitriiofficial@yandex.ru>
2025-07-01 09:08:34 +0300

    Add migration 2 - refs

---
+++ migrations/000002_references.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS role_permissions;
+DROP TABLE IF EXISTS permissions;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS ticket_priorities;
+DROP TABLE IF EXISTS ticket_statuses;
+DROP TABLE IF EXISTS departments;
